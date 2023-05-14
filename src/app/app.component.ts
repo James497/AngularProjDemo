0607eab0de3f33a74dec0635dcc4e3269bab4890
James497 <jamesehembrom@gmail.com>
2023-05-14 11:49:28 +0530

    templating practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Hello there! {{title}}</h1>
+  <ul>
+    <li *ngFor="let val of myArr">{{val}}</li>
+  </ul>
+  <div *ngIf="str == 'something'; then templ1 else templ2">
+  </div>
+  <ng-template #templ1 > Comparison is true</ng-template>
+  <ng-template #templ2 > Comparison is false</ng-template>
+  `,
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'AngularProjDemo';
+  myArr = ['orange', 'apple', 'guava'];
+  str = "something";
 }
